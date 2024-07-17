fbc5eba7336b40a3afec0c5f7ba76645c4180c66
Clemence <clemence@gsgasset.com>
2024-07-17 17:18:32 +0800

    init structure with zustand store and base structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import "./globals.css";
+import {Providers} from "./providers";
+
+const inter = Inter({subsets: ["latin"]});
+
+export const metadata: Metadata = {
+    title: "Pomodoro timer",
+    description: "timer for the pomodoro method of living life efficientlu",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
+}
